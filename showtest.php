8d5fa61a84414fa721b949dd3ec10ab79b988448
63070014 <73680260+63070014@users.noreply.github.com>
2020-11-27 10:13:38 +0700

    Update showtest.php

--- showtest.php
+++ showtest.php
@@ -39,7 +39,7 @@ while($Result = mysqli_fetch_array($res))
   <tr>
     <td><center><?php echo $Result['Product'];?></center></td>
     <td><center><?php echo $Result['Price'];?></center></td>
-    <td><center><?php echo $Result['Amount'];?></center></td>
+    <td><center><?php echo $Result['Amout'];?></center></td>
     <td><center><?php echo $sum = $Result['Price'] * $Result['Amount'];?></center></td>
 <?php
 }
